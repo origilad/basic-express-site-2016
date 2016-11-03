ea6c5522da0b1bdf8b73dd6e0ee7ddf015f1a435
nhasjim <nhasjim@gmail.com>
2016-11-03 15:18:43 -0700

    played with a test json file on TravelLog. Put a data folder to contain all json objects

--- routes/TravelLog.js
+++ routes/TravelLog.js
@@ -1,6 +1,9 @@
+var data = require( '../data/test.json');
+
 exports.TravelLog = function(req, res){
   try {
-    res.render('TravelLog', {title : 'TravelLog'})
+    res.render('TravelLog', {title : 'TravelLog'});
+    console.log(data);
   } catch (e) {
     next(e)
   }
