9d58ee60689ee4bfd0021d17edb94f67acdf6d9d
davidmoll <dj.moll24@gmail.com>
2016-11-09 11:27:00 -0800

    We can write inputted adventures to a big list of adventures stored in data.json

--- routes/LoggedAdventure.js
+++ routes/LoggedAdventure.js
@@ -1,14 +1,11 @@
 var jsonfile = require('jsonfile'); //so we can easily write to json
 jsonfile.spaces = 4; //so when we write to jsonfile it formats
-var filepath = __dirname + "/../data/test.json"; //read this json file
+var filepath = __dirname + "/../data/data.json";
 jsonContent = jsonfile.readFileSync(filepath); //read file and put as json object
-
-
 
 exports.LoggedAdventure = function(req, res){
   try {
     //renders (jade file, title you want to give it)
-    console.log(req);
     res.render('logged-adventure', {title : 'Logged Adventure'})
 
  } catch (e) {
@@ -18,15 +15,8 @@ exports.LoggedAdventure = function(req, res){
 
 
 exports.Submit = function(req, res) {
-
-  //Accessing json object. Has a key called adventures that is an array.
-  //Look at data/test.json for reference
-  var nextIndex = jsonContent.adventures.length //get the first open slot in the array
-  jsonContent.adventures[nextIndex] = { //adds it to next open spot in array
-    "name": "test2",
-    "image": "test2",
-    "id": "Test2"
-  }
-  //write back the stuff we just changed
-  jsonfile.writeFileSync(__dirname + '/../data/test.json', jsonContent);
+  console.log(req.body)
+  jsonContent['adventures'].push(req.body)
+  jsonfile.writeFileSync(__dirname + '/../data/TESTING.json', jsonContent); 
+  res.redirect('/Home');
 }
