127d29f8d74b29f9fc7d2bf6b2bf537493d349bc
Ori Gilad <ori.gilad96@gmail.com>
2016-11-10 17:55:51 -0800

    Added analytics - graph shows on every page, past week isn't complete

--- routes/AdventureAnalytics.js
+++ routes/AdventureAnalytics.js
@@ -10,11 +10,49 @@ exports.AdventureAnalytics = function(req, res){
   try {
     var user = req.params.user;
     for(var i = 0; i<jsonContent.users.length; i++){
-       console.log("wow");
-       console.log(user + "!");
        if(jsonContent.users[i].id === user){
+         var highestRatedAdventures = [];
+         var mostAdventures = [];
+         var highestRating = 0;
+         var highestVisit = 0;
+         var mostHours = [];
+         var highestHours = 0;
+         var message;
+         for(var j = 0; j<jsonContent.users[i].adventures.length; j++){
+            if(parseInt(jsonContent.users[i].adventures[j].Rating) > highestRating){
+              highestRating = jsonContent.users[i].adventures[j].Rating;
+              highestRatedAdventures = [];
+              highestRatedAdventures[0] = jsonContent.users[i].adventures[j].name;
+            }  
+            else if(jsonContent.users[i].adventures[j].Rating ===  highestRating ){
+              highestRatedAdventures[highestRatedAdventures.length] = jsonContent.users[i].adventures[j].name;
+            }
+            if(parseInt(jsonContent.users[i].adventures[j].Times_Visited) > highestVisit){
+              highestVisit = jsonContent.users[i].adventures[j].Times_Visited;
+              mostAdventures = [];
+              mostAdventures[0] = jsonContent.users[i].adventures[j].name;
+            }  
+            else if(jsonContent.users[i].adventures[j].Times_Visited ===  highestVisit ){
+              mostAdventures[mostAdventures.length] = jsonContent.users[i].adventures[j].name;
+            }
+            if(parseInt(jsonContent.users[i].adventures[j].Hours_Spent) > highestHours){
+              highestHours = jsonContent.users[i].adventures[j].Hours_Spent;
+              mostHours = [];
+              mostHours[0] = jsonContent.users[i].adventures[j].name;
+            }  
+            else if(jsonContent.users[i].adventures[j].Hours_Spent ===  highestHours ){
+              mostHours[mostHours.length] = jsonContent.users[i].adventures[j].name;
+            }
+
+            if(jsonContent.users[i].adventures.length < 10){
+              message = "Oh No! You've gone on less than 10 adventures, you should go out more!";
+            }
+            else { message = "You've logged more than 10 adventures, look at you ;)!";}
+            console.log(jsonContent.users[i].adventures.length + "%%%%");
+         }
+
          
-         res.render('AdventureAnalytics', {title: 'AdventureAnalytics', user: jsonContent.users[i]});
+         res.render('AdventureAnalytics', {title: 'AdventureAnalytics', user: jsonContent.users[i], highestRatedAd: highestRatedAdventures, mostVisited: mostAdventures, mostHours: mostHours, message: message});
 
 
        }
