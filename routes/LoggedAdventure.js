f23770c86fdadf2bc6fbd98559f98e4e92d5befc
Ori Gilad <ori.gilad96@gmail.com>
2016-11-28 21:49:41 -0800

    Added new fields

--- routes/LoggedAdventure.js
+++ routes/LoggedAdventure.js
@@ -2,18 +2,20 @@ var jsonfile = require('jsonfile'); //so we can easily write to json
 jsonfile.spaces = 4; //so when we write to jsonfile it formats
 var filepath = __dirname + "/../data/data.json";
 jsonContent = jsonfile.readFileSync(filepath); //read file and put as json object
+var emoji = require('node-emoji');
 
 exports.LoggedAdventure = function(req, res){
   try {
     //renders (jade file, title you want to give it)
-
+    //var emojis = [emoji.smile, emoji.crying_cat_face, emoji.stuck_out_tongue_closed_eyes, emoji.sleeping]; 
+    var emojis = [emoji.smile, '1']
     var user = req.params.user;
     var lat = req.params.lat;
     var lng = req.params.lng;
     for(var i = 0; i<jsonContent.users.length; i++){
        if(jsonContent.users[i].id === user){
 
-         res.render('logged-adventure', {lat: lat, lng:lng, title: 'Logged Adventure', user: jsonContent.users[i], values: []});
+         res.render('logged-adventure', {lat: lat, lng:lng, title: 'Logged Adventure', user: jsonContent.users[i], values: [], emojis: emojis});
        }
     }
  } catch (e) {
